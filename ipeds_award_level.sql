465c341a0913d4a12ce5ef164a8771bb0bdb5ee4
Justin Pfeifer <Justin.Pfeifer@dixie.edu>
2020-06-29 15:29:10 -0600

    cleaned up code and syncing with master

--- ipeds_award_level.sql
+++ ipeds_award_level.sql
@@ -1,10 +1,11 @@
-with cte_max_term_code_eff as (select
-                                   shrdgmr_pidm as pidm,
-                                   max(smbpgen_term_code_eff) as max_smbpgen_term_code_eff,
-                                   shrdgmr_program
-                               from shrdgmr s1
-                               left join smbpgen s2 on smbpgen_program = shrdgmr_program
-                               group by shrdgmr_pidm, shrdgmr_program)
+with cte_max_term_code_eff as (
+    select
+        shrdgmr_pidm as pidm,
+        max(smbpgen_term_code_eff) as max_smbpgen_term_code_eff,
+        shrdgmr_program
+    from shrdgmr s1
+    left join smbpgen s2 on smbpgen_program = shrdgmr_program
+    group by shrdgmr_pidm, shrdgmr_program)
 
 select distinct
     s1.shrdgmr_pidm,
@@ -27,38 +28,9 @@ select distinct
     end ipeds_awrd_lvl
 from shrdgmr s1
 left join smbpgen s2 on s2.smbpgen_program = s1.shrdgmr_program
-left join cte_max_term_code_eff s3 on s3.pidm = s1.shrdgmr_pidm and s3.shrdgmr_program = s2.smbpgen_program
-where
--- and shrdgmr_term_code_ctlg_1 >= s2.smbpgen_term_code_eff
-    s3.max_smbpgen_term_code_eff = s2.smbpgen_term_code_eff
-  and shrdgmr_grad_date > to_date('30-JUN-19')
+inner join cte_max_term_code_eff s3 on s3.pidm = s1.shrdgmr_pidm and s3.shrdgmr_program = s2.smbpgen_program and
+                                       s3.max_smbpgen_term_code_eff = s2.smbpgen_term_code_eff
+where shrdgmr_grad_date > to_date('30-JUN-19')
   and shrdgmr_grad_date < to_date('01-JUL-20')
---   and shrdgmr_degc_code like 'C%'
   and shrdgmr_term_code_grad = '202020'
 order by shrdgmr_pidm;
-
-select *
-from shrdgmr
-where shrdgmr_term_code_grad = '202020';
-
-select
-    smbpgen_req_credits_overall,
-    s1.*
-from smbpgen s1
-where smbpgen_program = 'CERT-GENED';
-
-select distinct
-    shrdgmr_program,
-    max(smbpgen_term_code_eff) as max_smbpgen_term_code_eff,
-    smbpgen_req_credits_overall
-from shrdgmr s1
-left join smbpgen s2 on smbpgen_program = shrdgmr_program
-where smbpgen_term_code_eff is null
-      --and shrdgmr_degc_code like 'C%'
-      --and shrdgmr_term_code_grad in ('201930', '201940', '202020' )
-group by shrdgmr_program, smbpgen_req_credits_overall
-order by 1;
-
-select *
-from smbpgen
-where smbpgen_program = 'CERT-GENED';
